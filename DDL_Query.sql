88b31a94670145c846aa5ddafd57943187b4104c
nando <muhammad.rezki.ananda@gmail.com>
2021-06-18 09:11:18 +0700

    Change data structure

--- DDL_Query.sql
+++ DDL_Query.sql
@@ -4,38 +4,10 @@ CREATE DATABASE db_inlaundry;
 USE db_inlaundry;
 
 -- Create TABLE 
-CREATE TABLE Cuci_Kiloan(
-    kode_cuci VARCHAR(11) PRIMARY KEY NOT NULL,
-    id_customers VARCHAR(11) NOT NULL,
-    kaos INT(3),
-    kemeja INT(3),
-    dress INT(3),
-    celana_pendek INT(3),
-    celana_panjang INT(3)
-);
-
-CREATE TABLE Jenis_Layanan(
-    id_layanan VARCHAR(11) PRIMARY KEY NOT NULL,
-    nama_layanan TEXT NOT NULL,
-    harga INT(11) NOT NULL,
-    id_owner VARCHAR(11) NOT NULL
-);
-
-CREATE TABLE Pemesanan(
-    id_order VARCHAR(11) PRIMARY KEY NOT NULL,
-    kode_cuci VARCHAR(11) NOT NULL,
-    id_layanan VARCHAR(11) NOT NULL
-);
-
 CREATE TABLE Owner(
     id_owner VARCHAR(11) PRIMARY KEY NOT NULL,
     username VARCHAR(11) NOT NULL,
-    email TEXT NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20),
-    date_of_birth DATE NOT NULL,
-    phone VARCHAR(14) NOT NULL
+    password VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Employee(
@@ -60,6 +32,31 @@ CREATE TABLE Customers(
     phone VARCHAR(14) NOT NULL
 );
 
+
+CREATE TABLE Cuci_Kiloan(
+    kode_cuci INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_customers VARCHAR(11) NOT NULL,
+    id_employee VARCHAR(11) NOT NULL,
+    kaos INT(3),
+    kemeja INT(3),
+    dress INT(3),
+    celana_pendek INT(3),
+    celana_panjang INT(3)
+);
+
+CREATE TABLE Jenis_Layanan(
+    id_layanan VARCHAR(11) PRIMARY KEY NOT NULL,
+    nama_layanan TEXT NOT NULL,
+    harga INT(11) NOT NULL,
+    id_owner VARCHAR(11) NOT NULL
+);
+
+CREATE TABLE Pemesanan(
+    id_order VARCHAR(11) PRIMARY KEY NOT NULL,
+    kode_cuci INT(11) NOT NULL,
+    id_layanan VARCHAR(11) NOT NULL
+);
+
 CREATE TABLE Status(
     id_status INT(2) PRIMARY KEY NOT NULL,
     status TEXT NOT NULL
@@ -71,7 +68,7 @@ CREATE TABLE Pembayaran(
     id_customers VARCHAR(11) NOT NULL,
     id_employee VARCHAR(11) NOT NULL,
     id_status INT(2) NOT NULL,
-    tanggal_transaksi DATE NOT NULL,
+    tanggal_transaksi DATETIME NOT NULL,
     berat INT(3) NOT NULL,
     total INT(11) NOT NULL
 );
@@ -83,6 +80,10 @@ FOREIGN KEY (id_owner) REFERENCES Owner(id_owner);
 
 ALTER TABLE Cuci_Kiloan
 ADD CONSTRAINT fk_cucikiloan_customers
+FOREIGN KEY (id_employee) REFERENCES Employee(id_employee);
+
+ALTER TABLE Cuci_Kiloan
+ADD CONSTRAINT fk_cucikiloan_employee
 FOREIGN KEY (id_customers) REFERENCES Customers(id_customers);
 
 ALTER TABLE Pemesanan
